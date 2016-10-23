bf08bf47fb216597ecda931b13002240d4876f69
Alan Jones <alan@Alans-MacBook-Pro.local>
2016-10-23 15:17:51 +0100

    Created lists app, extended func tests to cover 1-2 to do items

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 # Firefox does not work, not fully installed..
@@ -15,22 +16,50 @@ class NewVisitorTest(unittest.TestCase):
     def test_starting_a_new_todo_list(self):
         # Edith has heard of a great website on to to lists and goes to homepage
         self.browser.get('http://localhost:8000')
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
 
         # She notices the header mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
 
         # She is invited to enter a do-do right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a to-do
+        inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter,  the page updates and lists "1. Buy peacock feathers"
+        # When she hits enter, the page updates and lists "1. Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_name('tr')
+        self.assertIn(
+            "1: Buy peacock feathers",
+            [row.text for row in rows]
+        )
 
         # There is still a text box inviting her to enter another to-do
+        # She enters "Use feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_name('tr')
+        self.assertIn(
+            "2: Use feathers to make a fly",
+            [row.text for row in rows]
+        )
+        self.assertIn(
+            "1: Buy peacock feathers",
+            [row.text for row in rows]
+        )
 
         # Edith wonders whether the site will remember her list.  She sees there is a
         # unique url for her
+        self.fail('Finish the test')
 
         # She visits that url and her list is still there
 

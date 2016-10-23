f7201fee39d95eed21a6a712f59eef842f20108d
Alan Jones <alan@Alans-MacBook-Pro.local>
2016-10-23 15:53:58 +0100

    Using template for home page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -9,7 +9,14 @@ class HomePageTest(TestCase):
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do List</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do List</title>', response.content)
+        # self.assertTrue(response.content.endswith(b'</html>'))
+
+        # Tests the home page uses the home.html template
+        with open('lists/templates/home.html') as f:
+            expected_content = f.read()
+        self.assertEqual(response.content.decode(), expected_content)
+
 

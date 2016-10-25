5bb4c6875ec99c9a294912b29a1acc30e874bb30
Alan Jones <alan@Alans-MacBook-Pro.local>
2016-10-25 17:25:11 +0100

    Add /lists for URL but still one global list

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page
+from lists.views import home_page, list_view
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', list_view, name='list_view'),
     url(r'^admin/', admin.site.urls),
 
 ]

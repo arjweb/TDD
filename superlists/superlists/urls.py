313bbc03a8d9f3aa11455a689ef553a872d96464
Alan Jones <alan@Alans-MacBook-Pro.local>
2016-10-26 22:16:19 +0100

    Supporting more than one list

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page, list_view, new_list
+from lists.views import home_page, list_view, new_list, add_item
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', list_view, name='list_view'),
+    url(r'^lists/(\d+)/$', list_view, name='list_view'),
     url(r'^lists/new$', new_list, name='new_list'),
     url(r'^admin/', admin.site.urls),
+    url(r'^lists/(\d+)/add$', add_item, name='add_item'),
 
 ]

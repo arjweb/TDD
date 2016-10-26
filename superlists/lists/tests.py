198798dfc7bde711115cdd605d0a1b1634d1939d
Alan Jones <alan@Alans-MacBook-Pro.local>
2016-10-26 16:02:21 +0100

    Created List model and linked with Item

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your tests here.
@@ -47,8 +47,9 @@ class NewListViewTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_lists_page_shows_items_in_database(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
         # Django helper
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertContains(response, 'item 1')
@@ -61,15 +62,23 @@ class ListViewTest(TestCase):
 
 class ItemModelText(TestCase):
     def test_saving_and_retrieving_items_in_database(self):
+        first_list = List()
+        first_list.save()
+
         first_item = Item()
         first_item.text = 'Item the first'
+        first_item.list = first_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'second item'
+        second_item.list = first_list
         second_item.save()
 
         first_item_from_db = Item.objects.all()[0]
         self.assertEqual(first_item_from_db.text, 'Item the first')
+        self.assertEqual(first_item_from_db.list, first_list)
+
         second_item_from_db = Item.objects.all()[1]
         self.assertEqual(second_item_from_db.text, 'second item')
+        self.assertEqual(second_item_from_db.list, first_list)
